f17dcdb2106d8a085b69e18a2c2fbeb8cf5d7531
NourAllah Ahdy haggag <nourallah.haggag@robustastudio.com>
2021-01-02 20:47:33 +0200

    add Dependencies and versions files

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,103 @@
+/**
+ * Created by Mohamed Shalan on 3/15/20.
+ */
+
+object Versions {
+
+    val instaDots = "1.1"
+
+    val googleWallet = "16.0.1"
+
+	val imageCompressor = "2.1.0"
+
+    val firebaseAppDistributionClasspath = "2.0.1"
+
+	val firebasePlatform = "25.12.0"
+
+	val firebaseCloudMessaging = "20.2.4"
+
+	val firebaseDynamicLinks = "19.1.0"
+
+    val firebaseCrashlytics = "17.1.1"
+
+	val firebaseCrashlyticsClassPath = "2.3.0"
+
+	val firebaseAnalytics = "17.2.2"
+
+	val googleServiceClassPath = "4.3.3"
+
+    val tedImagePicker = "1.1.3"
+
+    val customTab = "1.2.0"
+
+	val wallet = "16.0.1"
+
+    val tedPermission = "2.2.3"
+
+    val viewpager2 = "1.0.0"
+
+    val validator = "2.0.3"
+
+    val shimmerLayout = "2.1.0"
+
+	val leakCanary = "2.3"
+
+	val skeletonLayout = "1.1.2"
+
+	val googleMaterial = "1.1.0"
+
+	val recyclerview = "1.2.0-alpha02"
+
+	val rxKotlin = "3.0.0"
+
+	val hyperion = "0.9.27"
+
+	val chuckerVersion = "3.2.0"
+
+	val glide = "4.11.0"
+
+	val navigation_component = "2.3.0-beta01"
+
+	val moshi_kotlin = "1.9.2"
+
+	val logging_interceptor = "4.5.0"
+
+	val retrofit = "2.8.1"
+
+	val rxjava3 = "3.0.0"
+
+	val rxandroid = "3.0.0"
+
+	val koin = "2.1.6"
+
+	val lifecycle = "2.2.0"
+
+	val constraint_layout = "2.0.4"
+
+	val compile_sdk = 29
+
+	val min_sdk = 21
+
+	val target_sdk = 29
+
+	val build_tools_version = "29.0.3"
+
+	val version_code = 22
+
+	val version_name = "1.3.1"
+
+	val kotlin_version = "1.4.10"
+
+	val gradle_version = "4.1.1"
+
+	val app_compact = "1.2.0"
+
+	val core_ktx = "1.2.0"
+
+	val junit = "4.13"
+
+	val androidx_test_junit = "1.1.1"
+
+	val androidx_espresso = "3.2.0"
+
+}
