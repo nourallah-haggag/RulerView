fb37a8d245ecfdd8b9adceb3a718d6d2c165cecf
NourAllah Ahdy haggag <nourallah.haggag@robustastudio.com>
2021-01-11 18:43:11 +0200

    attach latest requests recycler to pager snap helper

--- app/src/main/java/com/rumbl/rumbl_pt/features/home/home_list/LatestRequestsItemViewHolder.kt
+++ app/src/main/java/com/rumbl/rumbl_pt/features/home/home_list/LatestRequestsItemViewHolder.kt
@@ -1,6 +1,7 @@
 package com.rumbl.rumbl_pt.features.home.home_list
 
 import android.view.View
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.rumbl.rumbl_pt.features.home.home_list.latest_requests.LatestRequestsAdapter
 import com.rumbl.rumbl_pt.models.LatestSessionsRequests
 import kotlinx.android.synthetic.main.item_latest_requests.view.*
@@ -13,6 +14,8 @@ class LatestRequestsItemViewHolder(itemView: View) : HomeItemsViewHolder(itemVie
                 val sessionsAdapter =
                     LatestRequestsAdapter(item.sessions, homeItemsInteractionListener)
                 rv_sessions_latest_requests.adapter = sessionsAdapter
+                val pagerSnapHelper = PagerSnapHelper()
+                pagerSnapHelper.attachToRecyclerView(rv_sessions_latest_requests)
             }
         }
     }
